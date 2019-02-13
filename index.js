77b749f239d29eb9f74bfa04bff8114205bfd4e8
Bela <borsy1397@gmail.com>
2019-02-13 12:17:40 +0100

    Add swagger to backend. Create folders to middlewares, routes and models

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./assets/swagger_v1.json');
+
 
 const app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res, next) => {
+    res.send('Tic-Tac-Toe Game');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Error!");
+});
+
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Listening....${port}`);
+    console.log(`Listening... Port: ${port}`);
 });
