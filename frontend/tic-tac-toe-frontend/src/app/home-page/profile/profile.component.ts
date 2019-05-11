9c498c93426d70d0d5dafc1ec245c85899612f09
Bela <borsy1397@gmail.com>
2019-05-11 11:05:54 +0200

    profile

--- frontend/tic-tac-toe-frontend/src/app/home-page/profile/profile.component.ts
+++ frontend/tic-tac-toe-frontend/src/app/home-page/profile/profile.component.ts
@@ -22,6 +22,38 @@ export class ProfileComponent implements OnInit {
       console.log(this.user);
     });
   }
-  
 
+  joined(date: string) {
+    return date.substr(0, 10);
+  }
+
+  result(win: boolean, draw: boolean) {
+    if (draw) {
+      return 'DRAW';
+    } else if (win) {
+      return 'WON';
+    } else {
+      return 'LOST';
+    }
+  }
+
+  resColor(win: boolean, draw: boolean) {
+    if (draw) {
+      return '#f0ad4e';
+    } else if (win) {
+      return '#5cb85c';
+    } else {
+      return '#d9534f';
+    }
+  }
+
+  time(seconds: number) {
+    let minutes = Math.floor(seconds / 60);
+    let sec = seconds - minutes * 60;
+    return this.str_pad_left(minutes,'0',2)+':'+ this.str_pad_left(sec,'0',2);
+  }
+
+  str_pad_left(string, pad, length) {
+    return (new Array(length + 1).join(pad) + string).slice(-length);
+  }
 }
